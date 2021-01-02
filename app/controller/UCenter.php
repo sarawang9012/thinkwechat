716b46647c59e3d4ffd29832799995cdc9d8d68e
sarawang9012 <625504790@qq.com>
2021-01-02 16:35:51 +0800

    1. added ucenter support 2 communication successful with remote ucenter server

---
+++ app/controller/UCenter.php
@@ -0,0 +1,12 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\controller;
+
+
+use Ucenter\Controller\ApiController;
+class UCenter extends ApiController
+{
+
+    public $eventListener = 'app\model\uc\UCenter';
+}
---
+++ app/model/uc/UCenter.php
@@ -0,0 +1,14 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\model\uc;
+use UCenter\Model\EventModel;
+
+
+/**
+ * @mixin \think\Model
+ */
+class UCenter extends EventModel
+{
+    //
+}
